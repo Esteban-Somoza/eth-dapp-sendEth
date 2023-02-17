82371b2b1a0e3d97bf9c5d037ffb6a93a8e7cd6b
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-17 12:17:55 -0300

    welcome with loader

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,10 +4,24 @@ import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle } from 'react-icons/bs'
 import Loader from './Loader'
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step='0.0001'
+    value={value}
+    onChange={e => handleChange(e, name)}
+    className='my-2 w-full rounded-sm p-2 outline-none bh-transparent text-white border-none text-sm white-glassmorphism' />
+)
+
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 export default function Welcome() {
   const connectWallet = () => {
+
+  }
+
+  const handleSubmit = () => {
 
   }
 
@@ -39,8 +53,36 @@ export default function Welcome() {
         </div>
         <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
           <div className='p-3 justitfy-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
-            
+            <div className="flex justify-between flex-col w-fill h-full">
+              <div className='flex  justify-between items-start'>
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color='#fff' />
+                </div>
+                <BsInfoCircle fontSize={17} color='#fff' />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm ">
+                  0x7123hih4aiddfufvh98h2339rh2
+                </p>
+                <p className="text-white font-semibold text-lg mt-1 ">
+                  Ethereum
+                </p>
+              </div>
+            </div>
           </div>
+          <form className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+            <Input placeholder='Address To' name='addressTo' type='text' handleChange={() => { }} />
+            <Input placeholder='Amount (Eth)' name='amount' type='number' handleChange={() => { }} />
+            <Input placeholder='Keyword (gif)' name='keyword' type='text' handleChange={() => { }} />
+            <Input placeholder='Enter message' name='message' type='text' handleChange={() => { }} />
+            <div className='h-[1px] w-full bg-gray-400 my-2' />
+            {true ? (<Loader />) :
+              (
+                <button type='button' onClick={handleSubmit} className='text-white w-fill mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursos-pointer'>
+                  Send Now
+                </button>
+              )}
+          </form>
         </div>
       </div>
     </div>
