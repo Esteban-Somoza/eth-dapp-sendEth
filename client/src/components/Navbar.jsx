7a805fdba6463495cce236c77195f00299c0019f
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-17 10:39:30 -0300

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { HiMenuAlt4 } from 'react-icons/hi'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FaBeer } from "react-icons/fa";
@@ -22,19 +22,19 @@ export default function Navbar() {
         <img src={logo} alt="logo" className='w-32 cursor-pointer' />
 
       </div>
-      <ul className='text-white md:flex list-none flex-row justify-between items-center flex-initial'>
+      <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => {
           return <NavbarItem key={item + index} title={item} />
         })}
-        <li className='bg-[#2952e3] text-inherit py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>Login</li>
-        {/* <li className='bg-[#2952e3] text-inherit py-2 px-7 mx-4 rounded-full cursor-pointer hover:gb-[#2546bd]'></li> */}
-      </ul>
-      <div className='flex relative'>
-        {
-          toggleMenu ?
-            <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
-            : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)} />
-        }
+        <li className='b0z-1>
+            <li className='text-xl w-full my-2'>
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => {
+              return <NavbarItem key={item + index} title={item} classProps='my-2 text-lg'/>
+            })}
+          </ul>
+        )}
       </div>
     </nav>
   )
