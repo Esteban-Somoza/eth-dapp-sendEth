3ab35fb3b4f8ae725cc88104ed0f0493f521b04f
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-17 13:25:56 -0300

    welcome and update tailwind config

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -27,8 +27,8 @@ export default function Welcome() {
 
   return (
     <div className='flex w-full justify-center items-center'>
-      <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
-        <div className='flex flex-1 justify-start flex-col md:mr-10'>
+      <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
+        <div className='flex flex-1 justify-start flex-col mf:mr-10'>
           <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
             Send Crypto
             <br />
@@ -51,7 +51,7 @@ export default function Welcome() {
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
-        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
           <div className='p-3 justitfy-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
             <div className="flex justify-between flex-col w-fill h-full">
               <div className='flex  justify-between items-start'>
