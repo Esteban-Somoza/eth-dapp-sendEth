45da0df1976e2fc7ee4ddb1b3abe259692bdaf6c
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-17 14:18:35 -0300

    creation of utils folder

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0xd57693Ea6E55d8c71e5D2AAa4e93E27aD8b4B2C3'
--- smartContract/hardhat.config.js
+++ smartContract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/p8cDegGrNYRuWlXQOCwAOj9g_mUBu7CT
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/p8cDegGrNYRuWlXQOCwAOj9g_mUBu7CT',
+      accounts: ['bd0c1cc21d6c15c98f041673391e90eba44f958c1ddc917c451f7fcfd26d7610']
+    }
+  }
+}
