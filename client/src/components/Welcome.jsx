20d9bcd67d6389cf18f1cf9bffcab410d696c164
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-17 15:04:20 -0300

    created welcome and state

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { AiFillPlayCircle } from 'react-icons/ai'
 import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle } from 'react-icons/bs'
+
+import { TransactionContext } from '../context/TransactionContext'
 import Loader from './Loader'
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
@@ -17,12 +19,15 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 export default function Welcome() {
-  const connectWallet = () => {
+  const { connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } = useContext(TransactionContext);
 
-  }
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+    e.preventDefault();
 
-  const handleSubmit = () => {
+    if (!addressTo || !amount || keyword || message) return
 
+    sendTransaction();
   }
 
   return (
@@ -37,11 +42,13 @@ export default function Welcome() {
           <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypt
           </p>
-          <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-            <p className='text-white text-base font-semibold'>
-              Connect Wallet
-            </p>
-          </button>
+          {
+            !currentAccount && <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+              <p className='text-white text-base font-semibold'>
+                Connect Wallet
+              </p>
+            </button>
+          }
           <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
             <div className={commonStyles}>Security</div>
@@ -62,7 +69,7 @@ export default function Welcome() {
               </div>
               <div>
                 <p className="text-white font-light text-sm ">
-                  0x7123hih4aiddfufvh98h2339rh2
+                  {currentAccount}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1 ">
                   Ethereum
@@ -71,10 +78,10 @@ export default function Welcome() {
             </div>
           </div>
           <form className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-            <Input placeholder='Address To' name='addressTo' type='text' handleChange={() => { }} />
-            <Input placeholder='Amount (Eth)' name='amount' type='number' handleChange={() => { }} />
-            <Input placeholder='Keyword (gif)' name='keyword' type='text' handleChange={() => { }} />
-            <Input placeholder='Enter message' name='message' type='text' handleChange={() => { }} />
+            <Input placeholder='Address To' name='addressTo' type='text' handleChange={handleChange} />
+            <Input placeholder='Amount (Eth)' name='amount' type='number' handleChange={handleChange} />
+            <Input placeholder='Keyword (gif)' name='keyword' type='text' handleChange={handleChange} />
+            <Input placeholder='Enter message' name='message' type='text' handleChange={handleChange} />
             <div className='h-[1px] w-full bg-gray-400 my-2' />
             {true ? (<Loader />) :
               (
