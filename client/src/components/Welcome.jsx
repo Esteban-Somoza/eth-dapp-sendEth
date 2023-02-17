7667d5a291c69504418452b3e5d2c6181b0790e8
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-17 11:57:49 -0300

    welcome page styling

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,8 @@ import { SiEthereum } from 'react-icons/si'
 import { BsInfoCircle } from 'react-icons/bs'
 import Loader from './Loader'
 
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+
 export default function Welcome() {
   const connectWallet = () => {
 
@@ -26,6 +28,19 @@ export default function Welcome() {
               Connect Wallet
             </p>
           </button>
+          <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
+            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+            <div className={commonStyles}>Security</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={commonStyles}>Low fees</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+          </div>
+        </div>
+        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+          <div className='p-3 justitfy-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
+            
+          </div>
         </div>
       </div>
     </div>
