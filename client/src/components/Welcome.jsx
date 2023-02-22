5dae26c81620e24e2677501e1ba0ad14ebdfbd20
Esteban-Somoza <estfersomoza@gmail.com>
2023-02-22 19:40:26 -0300

    UI tweeks

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -41,10 +41,10 @@ export default function Welcome() {
           }
           <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
-            <div className={commonStyles}>Security</div>
-            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
-            <div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
-            <div className={commonStyles}>Low fees</div>
+            <div className={`rounded-tr-2xl sm:rounded-none ${commonStyles}`}>Security</div>
+            <div className={`sm:rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`sm:rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+            <div className={`rounded-bl-2xl sm:rounded-none ${commonStyles}`}>Low fees</div>
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
